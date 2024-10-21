46541d3a93c640275a31574a24c124154a62af05
n.nasyrkanov <n.nasyrkanov@rsk.kg>
2024-10-21 11:26:35 +0600

    solution for  63 Unique Paths

--- 63. Unique Paths II/Solution.java
+++ 63. Unique Paths II/Solution.java
@@ -1,3 +1,27 @@
-public class Solution {
+class Solution {
+    
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
 
+        return uniqueSum(0, 0, obstacleGrid, dp, m, n);
+    }
+
+    private int uniqueSum(int i, int j, int[][] obstacleGrid, int[][] dp, int m, int n){
+         if(i >= m || j>= n)
+           return 0;
+         if(obstacleGrid[i][j] == 1)
+            return 0;
+         if(i == m-1 && j == n-1)
+            return 1;
+        if(dp[i][j] > 0)
+           return dp[i][j];
+        int right = uniqueSum(i, j+1, obstacleGrid, dp, m, n);
+        int down = uniqueSum(i+1, j, obstacleGrid, dp, m, n);
+        int result = right + down;
+        dp[i][j] = result;
+        return result;
+    }
 }
